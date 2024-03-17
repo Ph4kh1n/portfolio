fc02f770e7574f786d063c416b245755a504c52c
Ph4kh1n <phakhinnongthong@gmail.com>
2024-03-17 22:20:49 +0700

    Add via files

---
+++ script.js
@@ -0,0 +1,89 @@
+
+var typed = new Typed('#typedText', {
+	strings: ["Web Developer.", "C Programming.", "Photographer"],
+	typeSpeed: 30,
+	backSpeed: 30,
+	loop: true
+});
+
+(function($) { "use strict";
+		
+	//Page cursors
+
+    document.getElementsByTagName("body")[0].addEventListener("mousemove", function(n) {
+        t.style.left = n.clientX + "px", 
+		t.style.top = n.clientY + "px", 
+		e.style.left = n.clientX + "px", 
+		e.style.top = n.clientY + "px", 
+		i.style.left = n.clientX + "px", 
+		i.style.top = n.clientY + "px"
+    });
+    var t = document.getElementById("cursor"),
+        e = document.getElementById("cursor2"),
+        i = document.getElementById("cursor3");
+    function n(t) {
+        e.classList.add("hover"), i.classList.add("hover")
+    }
+    function s(t) {
+        e.classList.remove("hover"), i.classList.remove("hover")
+    }
+    s();
+    for (var r = document.querySelectorAll(".hover-target"), a = r.length - 1; a >= 0; a--) {
+        o(r[a])
+    }
+    function o(t) {
+        t.addEventListener("mouseover", n), t.addEventListener("mouseout", s)
+    }
+
+	
+	//About page
+	
+	$(".about-text").on('click', function () {
+		$("body").addClass("about-on");
+	});
+	$(".about-close").on('click', function () {
+		$("body").removeClass("about-on");
+	});
+
+	
+	//Contact page
+	
+	$(".contact-text").on('click', function () {
+		$("body").addClass("contact-on");
+	});
+	$(".contact-close").on('click', function () {
+		$("body").removeClass("contact-on");
+	});
+
+	
+	//Travel portfolio page
+	
+	$(".gallery").on('click', function () {
+		$("body").addClass("gallery-on");
+	});
+	$(".gallery-close").on('click', function () {
+		$("body").removeClass("gallery-on");
+	});
+
+	
+	//Wildlife portfolio page
+	
+	$(".projects").on('click', function () {
+		$("body").addClass("projects-on");
+	});
+	$(".projects-close").on('click', function () {
+		$("body").removeClass("projects-on");
+	});
+
+	
+	//Nature portfolio page
+	
+	$(".nature").on('click', function () {
+		$("body").addClass("nature-on");
+	});
+	$(".nature-close").on('click', function () {
+		$("body").removeClass("nature-on");
+	});
+
+	
+})(jQuery);
