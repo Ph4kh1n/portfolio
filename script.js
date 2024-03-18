2cf748b7157d89e0fdfe8f62446d2b7f9fc419da
Ph4kh1n <phakhinnongthong@gmail.com>
2024-03-18 22:43:55 +0700

    Updated 18/03/24

--- script.js
+++ script.js
@@ -6,6 +6,22 @@ var typed = new Typed('#typedText', {
 	loop: true
 });
 
+document.querySelector(".contact-text").addEventListener("click", function () {
+	document.querySelector(".contact-section").classList.remove("hide");
+});
+
+document.querySelector(".contact-close").addEventListener("click", function () {
+	document.querySelector(".contact-section").classList.add("hide");
+});
+
+document.querySelector(".about-text").addEventListener("click", function () {
+	document.querySelector(".about-section").classList.remove("hide");
+});
+
+document.querySelector(".about-close").addEventListener("click", function () {
+	document.querySelector(".about-section").classList.add("hide");
+});
+
 (function($) { "use strict";
 		
 	//Page cursors
@@ -87,3 +103,50 @@ var typed = new Typed('#typedText', {
 
 	
 })(jQuery);
+
+window.addEventListener('load', myInit, false);
+
+function myInit()
+{
+    if (titleElementExists() == true)
+        document.getElementsByTagName('title')[0].innerText = constructPageTitleStr();
+    else
+        addPageTitle();
+    /*
+        other initialization code here.
+    */
+}
+
+function titleElementExists()
+{
+    var titleEl = document.getElementsByTagName('title');
+    if (titleEl.length == 0)
+        return false;
+    return true;
+}
+
+
+function constructPageTitleStr()
+{
+    var locationStr = window.location.pathname;
+
+    // make sure the below line reflects the actual name of the file
+    // sets locationStr = "enhzflep"
+    locationStr = locationStr.replace("/index.html", "");
+
+    // get the position of the last / char in the string
+    var slashPos = locationStr.lastIndexOf("/");
+
+    // extract the string that remains after it
+    locationStr = locationStr.substr(slashPos+1);
+    return locationStr;
+}
+
+function addPageTitle()
+{
+    var locationStr = constructPageTitleStr();
+    var title = document.createElement('title');
+    var titleText = document.createTextNode( 'Page Title = ' + locationStr );
+    title.appendChild( titleText );
+    document.head.appendChild(title);
+}
